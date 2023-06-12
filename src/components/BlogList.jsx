897ae5c759fde891e1b0d11a3d477acea0437d6b
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-06-12 14:27:45 +0700

    function as props

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,11 +1,14 @@
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
   return (
     <>
       <h1 className="text-3xl font-bold mb-4 text-gray-800 text-center">{ title }</h1>
       <div className="blog-list">
         {blogs.map((blog) => (
           <div className="blog-preview mb-5 hover:cursor-pointer hover:shadow-lg px-4 py-6 rounded-lg bg-slate-50 hover:bg-slate-100 transition-all" key={blog.id}>
-            <h2 className="font-semibold text-2xl text-yellow-500">{blog.title}</h2>
+            <div className="flex justify-between">
+              <h2 className="font-semibold text-2xl text-yellow-500">{blog.title}</h2>
+              <button onClick={() => handleDelete(blog.id)} className="px-4 py-2 bg-red-300 rounded-sm text-sm hover:bg-red-500 hover:text-white">Delete</button>
+            </div>
             <p className="text-gray-800">Written by: {blog.author}</p>
           </div>
         ))}
