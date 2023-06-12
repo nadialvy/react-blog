99a150fe76ed2ecc5565260aba6474113eac4d5f
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-06-12 15:22:34 +0700

    conditional loading

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id != id);
@@ -11,17 +11,21 @@ const Home = () => {
   }
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-    .then(res => {
-      return res.json();
-    })
-    .then((data) => {
-      setBlogs(data);
-    })
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then((data) => {
+        setIsLoading(false);
+        setBlogs(data);
+      })
+    }, 1000)
   }, [])
 
   return (
     <div className="mx-32 mt-8">
+      {isLoading && <div className="text-center text-xl font-semibold text-gray-800">Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>}
     </div>
   );
