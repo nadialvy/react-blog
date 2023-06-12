62552360450a9040aaad66fd23772455a2dc9385
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-06-12 14:21:39 +0700

    reusing compoents

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,13 +1,16 @@
-const BlogList = ({ blogs }) => {
+const BlogList = ({ blogs, title }) => {
   return (
-    <div className="blog-list">
-      {blogs.map((blog) => (
-        <div className="blog-preview mb-5 hover:cursor-pointer hover:shadow-lg px-4 py-6 rounded-lg bg-slate-50 hover:bg-slate-100 transition-all" key={blog.id}>
-          <h2 className="font-bold text-2xl text-yellow-500">{blog.title}</h2>
-          <p className="text-gray-800 font-semibold">Written by: {blog.author}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <h1 className="text-3xl font-bold mb-4 text-gray-800 text-center">{ title }</h1>
+      <div className="blog-list">
+        {blogs.map((blog) => (
+          <div className="blog-preview mb-5 hover:cursor-pointer hover:shadow-lg px-4 py-6 rounded-lg bg-slate-50 hover:bg-slate-100 transition-all" key={blog.id}>
+            <h2 className="font-semibold text-2xl text-yellow-500">{blog.title}</h2>
+            <p className="text-gray-800">Written by: {blog.author}</p>
+          </div>
+        ))}
+      </div>
+    </>
    );
 }
 
