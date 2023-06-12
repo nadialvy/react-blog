9b9192b260f572ab94e30fa35259fd8c25fffc31
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-06-12 15:40:33 +0700

    handling fetch error

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id != id);
@@ -14,17 +15,24 @@ const Home = () => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
       .then(res => {
+        if(res.status >= 400) throw new Error("Server Error");
         return res.json();
       })
       .then((data) => {
-        setIsLoading(false);
         setBlogs(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        setError(e.message);
       })
     }, 1000)
   }, [])
 
   return (
     <div className="mx-32 mt-8">
+      {error && <div className="text-center text-xl font-semibold text-red-800">{error}</div>}
       {isLoading && <div className="text-center text-xl font-semibold text-gray-800">Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>}
     </div>
